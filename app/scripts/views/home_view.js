51805d32beb07aa4e5e1b2006e1747272bfc2547
rdanieldesign <rdanieldesign@gmail.com>
2015-02-01 11:37:36 -0500

    Content on a page

--- app/scripts/views/home_view.js
+++ app/scripts/views/home_view.js
@@ -2,8 +2,31 @@
 
 	App.Views.Home = Backbone.View.extend({
 
+		tagName: 'ul',
+		className: 'postList',
+
+		template: _.template($('#homePosts').html()),
+
 		initialize: function(){
 			console.log('Home View Initialized')
+
+			this.render();
+			$('#content').html(this.$el);
+		},
+
+		render: function(){
+
+			console.log(App.posts);
+
+			var self = this;
+			var posts = App.posts.models;
+
+			this.$el.empty();
+
+			_.each(posts, function(x){
+				var post = self.template(x.attributes);
+				self.$el.append(post);
+			});
 		}
 
 	});
